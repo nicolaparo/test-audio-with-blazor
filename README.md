4ecdf314ca43429e280c9d04f3513c697299afc0
Nicola Paro <nicola.paro@gmail.com>
2023-01-29 20:52:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Test Audio With Blazor
+
+This simple project illustrates how to control an HTML5 audio element with Blazor
+
---
+++ TestAudioWithBlazor/wwwroot/blazoraudio.js
@@ -0,0 +1,5 @@
+﻿var audio_play = (audio) => audio.play();
+var audio_pause = (audio) => audio.pause();
+var audio_setCurrentTime = (audio, timeInSeconds) => audio.currentTime = timeInSeconds;
+var audio_getCurrentTime = (audio) => audio.currentTime;
+var audio_getDuration = (audio) => audio.duration;
